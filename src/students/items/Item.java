75577fd0a17e3dc2ef7ae036ae2f9079e0d2da73
walwy004 <walwy004@mymail.unisa.edu.au>
2025-04-11 13:35:38 +0930

    added Item.java class constructor

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,18 @@
 package students.items;
 
-public class Item {
-
+public abstract class Item {
+	
+	protected int age;
+	protected int maturationAge;
+	protected int deathAge;
+	protected int value;
+	
+	public Item(int maturationAge, int deathAge, int value) {
+		this.age = 0;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+        this.value = value;
+        
+	}
 	
 }
