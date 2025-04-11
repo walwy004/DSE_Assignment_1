cd582aa5569f4de203f8bebfd1818b76a62a1067
walwy004 <walwy004@mymail.unisa.edu.au>
2025-04-11 22:37:15 +0930

    Added grid to Field.java

--- src/students/Field.java
+++ src/students/Field.java
@@ -1,10 +1,23 @@
 package students;
 
+import students.items.*;
 
 public class Field {
 	
-	public Field(int height, int width)
-	{
+	private Item[][] grid;
+	private int height;
+	private int width;
+	
+	public Field(int height, int width) {
+		this.height = height;
+		this.width = width;
+		grid = new Item[height][width];
+		
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				grid[row][col] = new Soil(); // Starts with all soil
+			}
+		}
 	}
 	
 }
