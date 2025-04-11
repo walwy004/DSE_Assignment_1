d6a7aacbba09bd81868919ff5f2aff0746345bd1
walwy004 <walwy004@mymail.unisa.edu.au>
2025-04-11 13:49:07 +0930

    added getters and setters to Items.java
    Change the Items.java attributes from protected to private.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -2,10 +2,10 @@ package students.items;
 
 public abstract class Item {
 	
-	protected int age;
-	protected int maturationAge;
-	protected int deathAge;
-	protected int value;
+	private int age;
+	private int maturationAge;
+	private int deathAge;
+	private int value;
 	
 	public Item(int maturationAge, int deathAge, int value) {
 		this.age = 0;
@@ -14,5 +14,27 @@ public abstract class Item {
         this.value = value;
         
 	}
+
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getMaturationAge() {
+		return maturationAge;
+	}
+
+	public int getDeathAge() {
+		return deathAge;
+	}
+
+	public int getValue() {
+		return value;
+	}
+	
+	
 	
 }
